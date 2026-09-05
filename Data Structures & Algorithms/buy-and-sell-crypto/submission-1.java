36bf756aa70acfdb1a406a4a24b9604dee4d4c27
Vejella Monvita Sai <23071a6663@vnrvjiet.in>
2026-09-06 00:28:34 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n= prices.length;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(prices[i] <prices[j])
+                {
+                    sum= Math.max(sum, prices[j]-prices[i]);
+                }
+            }
+        }
+        return sum;
+    }
+}
